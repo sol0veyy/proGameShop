a0b7b41dba3bab30df1d5bd657bf72fa3eee8329
Даниил Соловьев <solovevdanil197@gmail.com>
2023-12-11 13:28:02 +0300

    added some items product and footer

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    list_display_links = ('id', 'name')
+
+admin.site.register(Product, ProductAdmin)
